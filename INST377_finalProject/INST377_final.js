78bbc92d8f445c8d13e7cf3047728847780f943b
jnic11-ops <jnic11@terpmail.umd.edu>
2025-12-16 22:28:36 -0500

    Update INST377_final.js
    Added js for random button API

--- INST377_finalProject/INST377_final.js
+++ INST377_finalProject/INST377_final.js
@@ -1,1 +1,20 @@
+async function loadRandActiv() {
+    const response = await fetch("https://bored-api.appbrewery.com/random");
+    const data = await response.json();
+
+    const container = document.getElementById("randActivDiv");
+    const activName = document.getElementById("activName");
+    const activType = document.getElementById("activType");
+    const activParticipants = document.getElementById("activParticipants");
+    const activPrice = document.getElementById("activPrice");
+    const activLink = document.getElementById("activLink");
+
+    container.style.display = "block";
+
+    activName.textContent = `Activty: ${data.activity}`;
+    activType.textContent = `Type: ${data.type}`;
+    activParticipants.textContent = `Participants: ${data.participants}`;
+    activPrice.textContent = `Price: $ ${data.price}`;
+    activDuration.textContent = `Duration: ${data.duration}`;
+};
 
